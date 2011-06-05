c5b039aa7c79c7057ce306f3d9d3e08089adf51c
Flavio W Brasil <fwbrasil@fwbrasil.net>
2011-06-05 16:25:50 -0300

    removing lib directory

--- project/build/RadonStmProject.scala
+++ project/build/RadonStmProject.scala
@@ -5,11 +5,13 @@ class RadonStmProject(info: ProjectInfo) extends DefaultProject(info) {
 	val junit = "junit" % "junit" % "4.4"
 	val specs2 = "org.specs2" %% "specs2" % "1.3"
 	val scalaz = "org.specs2" %% "specs2-scalaz-core" % "6.0.RC2"
+	val commons_collections = "commons-collections" % "commons-collections" % "3.2.1"
 
 	def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
 	override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
 
 	val snapshots = "snapshots" at "http://scala-tools.org/repo-snapshots"
 	val releases = "releases" at "http://scala-tools.org/repo-releases"
-
+	val apache = "Maven" at "http://repo1.maven.org/maven2/"
+	
 }
