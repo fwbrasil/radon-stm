d6f833e83cc3f9f3cfff97bdce40a1ad5da460ce
Flavio W. Brasil <fwbrasil@gmail.com>
2011-11-26 22:09:10 -0200

    Organizing dependencies

--- project/build/RadonStmProject.scala
+++ project/build/RadonStmProject.scala
@@ -2,9 +2,9 @@ import sbt._
 
 class RadonStmProject(info: ProjectInfo) extends DefaultProject(info) {
 
-	val junit = "junit" % "junit" % "4.4"
-	val specs2 = "org.specs2" %% "specs2" % "1.3"
-	val scalaz = "org.specs2" %% "specs2-scalaz-core" % "6.0.RC2"
+	val junit = "junit" % "junit" % "4.4" % "test"
+	val specs2 = "org.specs2" %% "specs2" % "1.3" % "test"
+	val scalaz = "org.specs2" %% "specs2-scalaz-core" % "6.0.RC2" % "test"
 	val commons_collections = "commons-collections" % "commons-collections" % "3.2.1"
 
 	def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
