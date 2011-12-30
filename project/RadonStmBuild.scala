e91da52d17caa9f7c456a958e03bcc8f7c7a7f7f
Flavio W. Brasil <fwbrasil@gmail.com>
2011-12-30 18:35:12 -0200

    .

--- project/RadonStmBuild.scala
+++ project/RadonStmBuild.scala
@@ -9,32 +9,26 @@ object RadonStmBuild extends Build {
 	val scalaz = "org.specs2" %% "specs2-scalaz-core" % "6.0.RC2" % "test"
 	val commonsCollections = "commons-collections" % "commons-collections" % "3.2.1"
 	
-//	def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
-  	
   	/* Resolvers */
   	val customResolvers = Seq(
   	    "snapshots" at "http://scala-tools.org/repo-snapshots",
   	    "releases" at "http://scala-tools.org/repo-releases",
   	    "Maven" at "http://repo1.maven.org/maven2/"
   	)
-
+  	
     lazy val radonStm = 
     	Project(
     		id = "radon-stm",
     		base = file("."),
-    		settings = commonSettings ++ Seq(
+    		settings = Defaults.defaultSettings ++ Seq(
               libraryDependencies ++= 
-                  Seq(junit, specs2, scalaz, commonsCollections)
+                  Seq(junit, specs2, scalaz, commonsCollections),
+              publishTo := Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as("maven") withPermissions("0644")),
+    	      organization := "net.fwbrasil",
+    	      scalaVersion := "2.9.1",
+    	      version := "0.3-SNAPSHOT",
+    	      resolvers ++= customResolvers
             )
     	)
 
-    def commonSettings = 
-    	Defaults.defaultSettings ++ Seq(
-//    	    testFrameworks ++= Seq(specs2Framework),
-    	    publishTo := Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as("maven") withPermissions("0644")),
-    	    organization := "net.fwbrasil",
-    	    scalaVersion := "2.9.1",
-    	    version := "0.3-SNAPSHOT",
-    	    resolvers ++= customResolvers
-    	)
 }
