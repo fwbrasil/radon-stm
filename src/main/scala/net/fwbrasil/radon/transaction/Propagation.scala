cc78851ed417fc4dae7a208b28ded3e6300bbe99
Flavio W. Brasil <fwbrasil@gmail.com>
2012-02-07 14:37:01 -0200

    propagations review

--- src/main/scala/net/fwbrasil/radon/transaction/Propagation.scala
+++ src/main/scala/net/fwbrasil/radon/transaction/Propagation.scala
@@ -6,7 +6,13 @@ trait Propagation {
 	private[transaction] def execute[A](transaction: Option[Transaction])(f: => A)(implicit context: TransactionContext): A
 }
 
-class Required private[radon] extends Propagation {
+/**
+ * If the transaction attribute is Required, the container ensures that the enterprise bean's method will always be invoked with a JTA transaction.
+ * If the calling client is associated with a JTA transaction, the enterprise bean method will be invoked in the same transaction context.
+ * However, if a client is not associated with a transaction, the container will automatically begin a new transaction and try to commit the
+ * transaction when the method completes.
+ */
+class Required extends Propagation {
 	private[transaction] def execute[A](transaction: Option[Transaction])(f: => A)(implicit context: TransactionContext): A = {
 		import context.transactionManager._
 		if (transaction != None) {
@@ -20,14 +26,33 @@ class Required private[radon] extends Propagation {
 			runInNewTransactionWithRetry(f)
 	}
 }
+
+/**
+ * The transaction attribute Mandatory requires the container to invoke a bean's method in a client's transaction context.
+ * If the client is not associated with a transaction context when calling this method, the container throws
+ * javax.transaction.TransactionRequiredException if the client is a remote client or javax.ejb.TransactionRequiredLocalException
+ * if the client is a local client. If the calling client has a transaction context, the case is treated as Required by the container.
+ */
 class Mandatory extends Propagation {
 	private[transaction] def execute[A](transaction: Option[Transaction])(f: => A)(implicit context: TransactionContext): A = {
 		import context.transactionManager._
 		if (transaction == None)
 			throw new RequiredTransactionException
-		runInTransaction(transaction.get)(f)
+		val wasActive = isActive(transaction)
+		try {
+			runInTransaction(transaction.get)(f)
+		} finally
+			if (wasActive)
+				activate(transaction)
 	}
 }
+
+/**
+ * The transaction attribute Never requires that the enterprise bean method explicitly not be called within a transaction context.
+ * If the client calls with a transaction context, the container throws java.rmi.RemoteException if the client is a remote
+ * client or javax.ejb.EJBException if the client is a local client. If the client is not associated with any transaction context,
+ * the container invokes the method without initiating a transaction.
+ */
 class Never extends Propagation {
 	private[transaction] def execute[A](transaction: Option[Transaction])(f: => A)(implicit context: TransactionContext): A = {
 		import context.transactionManager._
@@ -36,6 +61,12 @@ class Never extends Propagation {
 		f
 	}
 }
+
+/**
+ * If the transaction attribute is NotSupported, the transactional context of the calling client is not propagated to the enterprise bean.
+ * If a client calls with a transaction context, the container suspends the client's transaction association before invoking the
+ * enterprise bean's method. After the method completes, the container resumes the suspended transaction association.
+ */
 class NotSupported extends Propagation {
 	private[transaction] def execute[A](transaction: Option[Transaction])(f: => A)(implicit context: TransactionContext): A = {
 		import context.transactionManager._
@@ -49,6 +80,13 @@ class NotSupported extends Propagation {
 				activate(transaction)
 	}
 }
+
+/**
+ * If the transaction attribute is RequiresNew, the container always creates a new transaction before invoking the enterprise bean
+ * method and commits the transaction when the method returns. If the calling client is associated with a transaction context, the
+ * container suspends the association of the transaction context with the current thread before starting the new transaction.
+ * When the method and the transaction complete, the container resumes the suspended transaction.
+ */
 class RequiresNew extends Propagation {
 	private[transaction] def execute[A](transaction: Option[Transaction])(f: => A)(implicit context: TransactionContext): A = {
 		import context.transactionManager._
@@ -62,6 +100,13 @@ class RequiresNew extends Propagation {
 				activate(transaction)
 	}
 }
+
+/**
+ * It the transaction attribute is Supports and the client is associated with a transaction context, the context is propagated
+ * to the enterprise bean method, similar to the way the container treats the Required case. If the client call is not
+ * associated with any transaction context, the container behaves similarly to the NotSupported case. The transaction
+ * context is not propagated to the enterprise bean method.
+ */
 class Supports extends Propagation {
 	private[transaction] def execute[A](transaction: Option[Transaction])(f: => A)(implicit context: TransactionContext): A = {
 		import context.transactionManager._
