cc2338069d556aca284c66da995834d29893e9ad
Flavio W. Brasil <fwbrasil@gmail.com>
2012-04-15 02:34:23 -0300

    Migrating to scala 2.9.2 release

--- project/RadonStmBuild.scala
+++ project/RadonStmBuild.scala
@@ -4,32 +4,31 @@ import Keys._
 object RadonStmBuild extends Build {
   
 	/* Dependencies */
-  	val junit = "junit" % "junit" % "4.4" % "test"
-	val specs2 = "org.specs2" %% "specs2" % "1.7" % "test"
-	val scalaz = "org.specs2" %% "specs2-scalaz-core" % "6.0.RC2" % "test"
+  val junit = "junit" % "junit" % "4.4" % "test"
+	val specs2 = "org.specs2" % "specs2_2.9.2-RC2" % "1.9" % "test"
 	val commonsCollections = "commons-collections" % "commons-collections" % "3.2.1"
 	
-  	/* Resolvers */
-  	val customResolvers = Seq(
-  	    "snapshots" at "http://scala-tools.org/repo-snapshots",
-  	    "releases" at "http://scala-tools.org/repo-releases",
-  	    "Maven" at "http://repo1.maven.org/maven2/"
+	/* Resolvers */
+	val customResolvers = Seq(
+    "snapshots" at "http://scala-tools.org/repo-snapshots",
+    "releases" at "http://scala-tools.org/repo-releases",
+    "Maven" at "http://repo1.maven.org/maven2/"
+	)
+	
+  lazy val radonStm = 
+  	Project(
+  		id = "radon-stm",
+  		base = file("."),
+  		settings = Defaults.defaultSettings ++ Seq(
+        libraryDependencies ++= 
+          Seq(junit, specs2, commonsCollections),
+//      publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository"))), 
+        publishTo := Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as("maven") withPermissions("0644")),
+	      organization := "net.fwbrasil",
+	      scalaVersion := "2.9.2",
+	      version := "0.8-SNAPSHOT",
+	      resolvers ++= customResolvers
+      )
   	)
-  	
-    lazy val radonStm = 
-    	Project(
-    		id = "radon-stm",
-    		base = file("."),
-    		settings = Defaults.defaultSettings ++ Seq(
-              libraryDependencies ++= 
-                  Seq(junit, specs2, scalaz, commonsCollections),
-//              publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository"))), 
-              publishTo := Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as("maven") withPermissions("0644")),
-    	      organization := "net.fwbrasil",
-    	      scalaVersion := "2.9.1",
-    	      version := "0.8-SNAPSHOT",
-    	      resolvers ++= customResolvers
-            )
-    	)
 
 }
