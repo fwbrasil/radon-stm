d05eb6691813ff931dcb1ce487410550fa62f3d4
Flavio W. Brasil <fwbrasil@gmail.com>
2012-08-21 13:42:26 -0300

    RC4

--- project/RadonStmBuild.scala
+++ project/RadonStmBuild.scala
@@ -27,7 +27,7 @@ object RadonStmBuild extends Build {
 	      organization := "net.fwbrasil",
 	      scalaVersion := "2.9.2",
         crossScalaVersions := Seq("2.9.1", "2.9.2"),
-	      version := "1.0-RC3",
+	      version := "1.0-RC4",
 	      resolvers ++= customResolvers
       )
   	)
