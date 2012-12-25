8f70e43b568a3e6bac806c5c8ac79464e698b2ac
Flavio W. Brasil <fwbrasil@gmail.com>
2012-12-25 12:23:11 -0200

    scala 2.10

--- project/RadonStmBuild.scala
+++ project/RadonStmBuild.scala
@@ -5,7 +5,8 @@ object RadonStmBuild extends Build {
 
 	/* Dependencies */
 	val junit = "junit" % "junit" % "4.4" % "test"
-	val specs2 = "org.specs2" %% "specs2" % "1.12.1" % "test"
+	val specs2 = "org.specs2" %% "specs2" % "1.13" % "test"
+	val scalaActors = "org.scala-lang" % "scala-actors" % "2.10.0"
 	val commonsCollections = "commons-collections" % "commons-collections" % "3.2.1"
 
 	/* Resolvers */
@@ -21,14 +22,14 @@ object RadonStmBuild extends Build {
 			base = file("."),
 			settings = Defaults.defaultSettings ++ Seq(
 				libraryDependencies ++=
-					Seq(junit, specs2, commonsCollections),
+					Seq(junit, specs2, commonsCollections, scalaActors),
 				// publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository"))), 
 				javacOptions ++= Seq("-source", "1.5", "-target", "1.5"),
 				publishTo := Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as ("maven") withPermissions ("0644")),
 				organization := "net.fwbrasil",
-				scalaVersion := "2.9.1",
-				crossScalaVersions := Seq("2.9.1", "2.9.2"),
-				version := "1.2-RC2",
+				scalaVersion := "2.10.0",
+				// crossScalaVersions := Seq("2.9.1", "2.9.2"),
+				version := "1.2-SNAPSHOT",
 				resolvers ++= customResolvers
 			)
 		)
