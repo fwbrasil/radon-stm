b2f5ce81d4a693eee6c7ef71885623f2e0e252ed
Flavio W. Brasil <fwbrasil@gmail.com>
2012-12-29 19:52:27 -0200

    performance regression test

--- src/test/scala/net/fwbrasil/radon/PerformanceTest.scala
+++ src/test/scala/net/fwbrasil/radon/PerformanceTest.scala
@@ -8,14 +8,23 @@ object RadonPerformanceTest
 
 	def persistor = new SerializationPersistor
 
-	val nums: Gen[Int] =
-		Gen.range("num")(0, 100000, 100)
+	val sizes: Gen[Int] =
+		Gen.range("size")(100, 10000, 100)
+
+	val ranges: Gen[Range] = for {
+		size <- sizes
+	} yield 0 until size
 
 	performance of "Ref" in {
 		measure method "empty constructor" in {
-			using(nums) in {
-				_ => transactional(new Ref[Int])
-			}
+			using(ranges) config (
+				exec.independentSamples -> 1) in {
+					range =>
+						transactional {
+							for (i <- range)
+								new Ref[Int]
+						}
+				}
 		}
 	}
 
