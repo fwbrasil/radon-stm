9622bec372c549fc8dbc8a8a15b5e499995a4de6
Flavio W. Brasil <fwbrasil@gmail.com>
2013-03-16 12:39:00 -0300

    release to maven central

--- project/RadonStmBuild.scala
+++ project/RadonStmBuild.scala
@@ -8,14 +8,6 @@ object RadonStmBuild extends Build {
 	val specs2 = "org.specs2" %% "specs2" % "1.13" % "test"
 	val scalaActors = "org.scala-lang" % "scala-actors" % "2.10.0" % "test"
 
-	/* Resolvers */
-	val customResolvers = Seq(
-		"snapshots" at "http://scala-tools.org/repo-snapshots",
-		"releases" at "http://scala-tools.org/repo-releases",
-		"Maven" at "http://repo1.maven.org/maven2/",
-		"Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
-	)
-
 	lazy val radonStm =
 		Project(
 			id = "radon-stm",
@@ -23,15 +15,45 @@ object RadonStmBuild extends Build {
 			settings = Defaults.defaultSettings ++ Seq(
 				libraryDependencies ++=
 					Seq(junit, specs2, scalaActors),
-				// publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository"))), 
 				javacOptions ++= Seq("-source", "1.5", "-target", "1.5"),
-				publishTo := Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as ("maven") withPermissions ("0644")),
+				// publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository"))), 
+				// publishTo := Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as ("maven") withPermissions ("0644")),
+				publishTo <<= version { v: String =>
+				  val nexus = "https://oss.sonatype.org/"
+				  if (v.trim.endsWith("SNAPSHOT")) 
+				    Some("snapshots" at nexus + "content/repositories/snapshots")
+				  else                             
+				    Some("releases" at nexus + "service/local/staging/deploy/maven2")
+				},
+				credentials += Credentials(Path.userHome / ".sbt" / "sonatype.credentials"),
+				publishMavenStyle := true,
+				publishArtifact in Test := false,
+				pomIncludeRepository := { x => false },
+				pomExtra := (
+				  <url>http://github.com/fwbrasil/radon-stm/</url>
+				  <licenses>
+				    <license>
+				      <name>LGPL</name>
+				      <url>https://github.com/fwbrasil/radon-stm/blob/master/LICENSE-LGPL</url>
+				      <distribution>repo</distribution>
+				    </license>
+				  </licenses>
+				  <scm>
+				    <url>git@github.com:fwbrasil/radon-stm.git</url>
+				    <connection>scm:git:git@github.com:fwbrasil/radon-stm.git</connection>
+				  </scm>
+				  <developers>
+				    <developer>
+				      <id>fwbrasil</id>
+				      <name>Flavio W. Brasil</name>
+				      <url>http://fwbrasil.net</url>
+				    </developer>
+				  </developers>
+				),
 				organization := "net.fwbrasil",
 				scalaVersion := "2.10.1",
 				crossScalaVersions := Seq("2.10.0", "2.10.1"),
-				version := "1.2",
-				resolvers ++= customResolvers,
-				testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework")
+				version := "1.2"
 			)
 		)
 
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,5 @@
 addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.1.1")
+
+resolvers += Resolver.url("sbt-plugin-releases", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
+
+addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8")
