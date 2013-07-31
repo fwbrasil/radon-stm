d86ba551614a9a44cb210a79bc9680d4da750567
Flavio W. Brasil <fwbrasil@gmail.com>
2013-07-31 12:12:14 -0300

    performance

--- src/main/scala/net/fwbrasil/radon/transaction/Transaction.scala
+++ src/main/scala/net/fwbrasil/radon/transaction/Transaction.scala
@@ -14,6 +14,7 @@ import net.fwbrasil.radon.ref.RefContent
 import net.fwbrasil.radon.util.ExclusiveThreadLocalItem
 import net.fwbrasil.radon.util.SemaphoreLockable.lockall
 import java.util.concurrent.atomic.AtomicLong
+import java.util.Collection
 
 class Transaction(val transient: Boolean)(implicit val context: TransactionContext)
         extends TransactionValidator
@@ -28,7 +29,7 @@ class Transaction(val transient: Boolean)(implicit val context: TransactionConte
     private var refsRead: ListBuffer[Ref[Any]] = _
     private var refsReadOnly: ListBuffer[Ref[Any]] = _
     private var refsWrite: ListBuffer[Ref[Any]] = _
-    private var snapshots: List[RefSnapshot] = _
+    private var snapshots: Collection[RefSnapshot] = _
     private var readLocks: List[Ref[Any]] = _
     private var writeLocks: List[Ref[Any]] = _
     var attachments = ListBuffer[Any]()
@@ -79,7 +80,7 @@ class Transaction(val transient: Boolean)(implicit val context: TransactionConte
         val refsRead = new ListBuffer[Ref[Any]]()
         val refsReadOnly = new ListBuffer[Ref[Any]]()
         val refsWrite = new ListBuffer[Ref[Any]]()
-        val snapshots = refsSnapshot.values.toList
+        val snapshots = refsSnapshot.values
         for (snapshot <- snapshots) {
             val ref = snapshot.ref
             if (snapshot.isRead) {
@@ -162,8 +163,9 @@ class Transaction(val transient: Boolean)(implicit val context: TransactionConte
 
     private def flushToMemory = {
         val snapshotsIterator = snapshots.iterator
-        snapshots = List()
-        snapshotsIterator.foreach(setRefContent)
+        snapshots = null
+        while (snapshotsIterator.hasNext)
+            setRefContent(snapshotsIterator.next)
     }
 
     private def validateTransaction = {
--- src/main/scala/net/fwbrasil/radon/util/SemaphoreLockable.scala
+++ src/main/scala/net/fwbrasil/radon/util/SemaphoreLockable.scala
@@ -4,7 +4,7 @@ import scala.collection._
 import java.util.concurrent.locks.ReentrantReadWriteLock
 import java.util.concurrent.locks.ReentrantLock
 import java.util.concurrent.TimeUnit
-import scala.collection.mutable.{ Set => MutableSet}
+import scala.collection.mutable.{ Set => MutableSet }
 import java.util.concurrent.atomic.AtomicReferenceArray
 import System.{ identityHashCode => identity }
 import java.util.concurrent.Semaphore
@@ -16,13 +16,13 @@ private[fwbrasil] trait SemaphoreLockable {
     private[fwbrasil] def tryReadLock =
         semaphore.tryAcquire(1)
 
-    private[fwbrasil] def tryWriteLock=
+    private[fwbrasil] def tryWriteLock =
         semaphore.tryAcquire(Int.MaxValue)
-    
-    private[fwbrasil] def readUnlock=
+
+    private[fwbrasil] def readUnlock =
         semaphore.release
 
-    private[fwbrasil] def writeUnlock=
+    private[fwbrasil] def writeUnlock =
         semaphore.release(Int.MaxValue)
 
 }
