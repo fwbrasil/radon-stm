4c68c116624f4bff1f7555a8ae4ec2046190f2b1
Flavio W. Brasil <fwbrasil@gmail.com>
2014-09-20 18:15:10 +0200

    remove cross compilation

--- project/RadonStmBuild.scala
+++ project/RadonStmBuild.scala
@@ -3,58 +3,53 @@ import Keys._
 
 object RadonStmBuild extends Build {
 
-	/* Dependencies */
-	val junit = "junit" % "junit" % "4.4" % "test"
-	val specs2 = "org.specs2" %% "specs2" % "2.3.13" % "test"
-	def scalaActors(scalaVersion: String) = "org.scala-lang" % "scala-actors" % scalaVersion % "test"
+    /* Dependencies */
+    val junit = "junit" % "junit" % "4.4" % "test"
+    val specs2 = "org.specs2" %% "specs2" % "2.3.13" % "test"
+    val scalaActors = "org.scala-lang" % "scala-actors" % "2.11.2" % "test"
 
-	lazy val radonStm =
-		Project(
-			id = "radon-stm",
-			base = file("."),
-			settings = Defaults.defaultSettings ++ Seq(
-				libraryDependencies ++=
-					Seq(junit, specs2),
-				libraryDependencies <+= scalaVersion(scalaActors(_)),
-				javacOptions ++= Seq("-source", "1.5", "-target", "1.5"),
-				// publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository"))), 
-				// publishTo := Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as ("maven") withPermissions ("0644")),
-				publishTo <<= version { v: String =>
-				  val nexus = "https://oss.sonatype.org/"
-				  if (v.trim.endsWith("SNAPSHOT")) 
-				    Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as("maven") withPermissions("0644"))
-				  else                             
-				    Some("releases" at nexus + "service/local/staging/deploy/maven2")
-				},
-				publishMavenStyle := true,
-				publishArtifact in Test := false,
-				pomIncludeRepository := { x => false },
-				pomExtra := (
-				  <url>http://github.com/fwbrasil/radon-stm/</url>
-				  <licenses>
-				    <license>
-				      <name>LGPL</name>
-				      <url>https://github.com/fwbrasil/radon-stm/blob/master/LICENSE-LGPL</url>
-				      <distribution>repo</distribution>
-				    </license>
-				  </licenses>
-				  <scm>
-				    <url>git@github.com:fwbrasil/radon-stm.git</url>
-				    <connection>scm:git:git@github.com:fwbrasil/radon-stm.git</connection>
-				  </scm>
-				  <developers>
-				    <developer>
-				      <id>fwbrasil</id>
-				      <name>Flavio W. Brasil</name>
-				      <url>http://fwbrasil.net</url>
-				    </developer>
-				  </developers>
-				),
-				organization := "net.fwbrasil",
-				scalaVersion := "2.11.2",
-    			crossScalaVersions := Seq("2.10.1", "2.11.2"),
-				version := "1.6"
-			)
-		)
+    lazy val radonStm =
+        Project(
+            id = "radon-stm",
+            base = file("."),
+            settings = Defaults.defaultSettings ++ Seq(
+                libraryDependencies ++=
+                    Seq(junit, specs2),
+                javacOptions ++= Seq("-source", "1.5", "-target", "1.5"),
+                // publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository"))), 
+                // publishTo := Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as ("maven") withPermissions ("0644")),
+                publishTo <<= version { v: String =>
+                    val nexus = "https://oss.sonatype.org/"
+                    if (v.trim.endsWith("SNAPSHOT"))
+                        Option(Resolver.ssh("fwbrasil.net repo", "fwbrasil.net", 8080) as ("maven") withPermissions ("0644"))
+                    else
+                        Some("releases" at nexus + "service/local/staging/deploy/maven2")
+                },
+                publishMavenStyle := true,
+                publishArtifact in Test := false,
+                pomIncludeRepository := { x => false },
+                pomExtra := (
+                    <url>http://github.com/fwbrasil/radon-stm/</url>
+                    <licenses>
+                        <license>
+                            <name>LGPL</name>
+                            <url>https://github.com/fwbrasil/radon-stm/blob/master/LICENSE-LGPL</url>
+                            <distribution>repo</distribution>
+                        </license>
+                    </licenses>
+                    <scm>
+                        <url>git@github.com:fwbrasil/radon-stm.git</url>
+                        <connection>scm:git:git@github.com:fwbrasil/radon-stm.git</connection>
+                    </scm>
+                    <developers>
+                        <developer>
+                            <id>fwbrasil</id>
+                            <name>Flavio W. Brasil</name>
+                            <url>http://fwbrasil.net</url>
+                        </developer>
+                    </developers>),
+                organization := "net.fwbrasil",
+                scalaVersion := "2.11.2",
+                version := "1.7"))
 
 }
